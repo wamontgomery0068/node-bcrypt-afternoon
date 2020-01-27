fd3b8877efd96f0d055ef298ea2aa73782065eab
Your Name <wamontgomery0068@gmail.com>
2020-01-27 07:39:48 -0700

    Database Setup

--- server/index.js
+++ server/index.js
@@ -5,8 +5,23 @@ const massive = require('massive');
 
 const PORT = 4000;
 
+const { SESSION_SECRET, CONNECTION_STRING } = process.env;
+
 const app = express();
 
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+massive(CONNECTION_STRING).then(db => {
+    app.set('db', db);
+    console.log('db connected');
+});
+
+app.use(
+    session({
+        resave: true,
+        saveUninitialized: false,
+        secret: SESSION_SECRET,
+    })
+);
+
+app.listen(PORT, () => console.log(`Boom Shakalaka! You are currently listening on Port ${PORT}`));
